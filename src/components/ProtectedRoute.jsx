ae770a7ae39e20e072ab665fb690d998c81b5d01
mhdi <mhdih4n@gmail.com>
2025-02-12 20:02:36 +0200

    roh takol roh

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
+import PropTypes from 'prop-types';
 
 const ProtectedRoute = ({ children, allowedRoles = [] }) => {
-  // const { user } = useAuth();
+  const { user } = useAuth();
 
   // Commenté temporairement pour accéder aux pages protégées
-  /*
+  
   if (!user) {
     return <Navigate to="/login" />;
   }
@@ -13,10 +14,15 @@ const ProtectedRoute = ({ children, allowedRoles = [] }) => {
   if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
     return <Navigate to="/unauthorized" />;
   }
-  */
+  
 
   // Retourne directement le contenu sans vérification
   return children;
+};
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+  allowedRoles: PropTypes.arrayOf(PropTypes.string)
 };
 
 export default ProtectedRoute;
